7ade4ea484d2018d5f3ae8c7eb19f8e88541c6ac
Kevin Thai <kevin.k.thai@gmail.com>
2020-02-01 18:14:21 -0600

    Updates

---
+++ precache-manifest.7aa4d1f344afbd294a45f6a1bc21b5f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a154280e9072c0ba2d8c",
+    "url": "/face-off/static/css/main.9df99699.chunk.css"
+  },
+  {
+    "revision": "a154280e9072c0ba2d8c",
+    "url": "/face-off/static/js/main.730ead56.chunk.js"
+  },
+  {
+    "revision": "703a5488f8d7196d8794",
+    "url": "/face-off/static/js/runtime~main.176b8b5e.js"
+  },
+  {
+    "revision": "59a59f06f42ead1679bd",
+    "url": "/face-off/static/css/2.28fd05b1.chunk.css"
+  },
+  {
+    "revision": "59a59f06f42ead1679bd",
+    "url": "/face-off/static/js/2.06a4712f.chunk.js"
+  },
+  {
+    "revision": "a2be062426ff2ce418b14d2152ac6e48",
+    "url": "/face-off/index.html"
+  }
+];
